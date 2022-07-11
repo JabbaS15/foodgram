94a9fa65489154c606a130094029b58fc83089ec
Jabba <JabbaS15@yandex.kz>
2022-07-11 22:30:05 +0600

    ver0.7.8.1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -119,10 +119,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = '/backend_static/'
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
 
 MEDIA_URL = '/media/'
 
